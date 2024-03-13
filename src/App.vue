b455961c73a5de0daf8ad87bef1e6d4abfb812c7
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-13 09:31:41 -0300

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import PrimeiroComponente from "@/components/PrimeiroComponente.vue";
+
+
+</script>
+
+<template>
+  <h1>ola mundo</h1>
+  <PrimeiroComponente/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {onMounted, ref} from "vue";
+
+const nome = ref('Henrique')
+
+
+onMounted(() => {
+  setTimeout(() =>{
+    nome.value = "Nascimento"
+  },2000)
+})
+
+</script>
+
+<template>
+  <h1>Meu nome é : {{nome}}</h1>
+</template>
